8057246cf152a9ab108e4f865b12d000847ec7b7
NagosC <121381818+NagosC@users.noreply.github.com>
2023-04-10 08:44:35 -0300

    declaring constants

--- sistema/configuracao.php
+++ sistema/configuracao.php
@@ -1,3 +1,9 @@
 <?php
-
+// Arquivo de configuração do Sistema
 date_default_timezone_set('America/Sao_Paulo');
+
+define('SITE_NOME', 'Unset');
+define('SITE_DESCRICAO', 'Unset - Tecnologia em Sistemas');
+
+define('URL_PRODUCAO', 'http://unset.com.br');
+define('URL_DESENVOLVIMENTO', 'http://localhost/blog');
